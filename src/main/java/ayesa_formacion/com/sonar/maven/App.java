fa54dfccb8c4528a7db1fe86808c58d4655bdb6a
itcon_2018 <info@itconsultoria.es>
2020-11-22 19:54:35 +0100

    maven project

---
+++ src/main/java/ayesa_formacion/com/sonar/maven/App.java
@@ -0,0 +1,13 @@
+package ayesa_formacion.com.sonar.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
